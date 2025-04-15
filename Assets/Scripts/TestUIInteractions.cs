e97dca59c750f5c00309999d3f6a3b8a1ae85937
Colin Logan <clogan36@gatech.edu>
2025-04-14 21:13:10 -0400

    Created starting environment for main menu and added ray interactions for controllers

---
+++ Assets/Scripts/TestUIInteractions.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestUIInteractions : MonoBehaviour
+{
+    // test
+    public void SayHi()
+    {
+        Debug.Log("Hi");
+    }
+}
