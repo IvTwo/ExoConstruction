3e153b35cff893ea39fc8ef8340e596548ea1b82
LilG00s3 <ekimmel8@gatech.edu>
2025-05-06 14:33:24 -0400

    Added moving brick in masonry

--- Assets/Scripts/PickupHandler.cs
+++ Assets/Scripts/PickupHandler.cs
@@ -6,8 +6,32 @@ public class PickupHandler : MonoBehaviour
 {
     [SerializeField] private Transform handTransform;
     [SerializeField] private Transform targetObjectTransform;
+    [SerializeField] private Transform attachTransform;
+    [SerializeField] private float lerpSpeed = 5f;
     private bool isFollowing = false;
+    private bool isMovingToTransform = false;
 
+
+    private void Update()
+    {
+        if (isMovingToTransform)
+        {
+            targetObjectTransform.position = Vector3.Lerp(
+                targetObjectTransform.position,
+                attachTransform.position,
+                Time.deltaTime * lerpSpeed);
+
+            targetObjectTransform.rotation = Quaternion.Slerp(
+                targetObjectTransform.rotation,
+                attachTransform.rotation,
+                Time.deltaTime * lerpSpeed);
+
+            if (Vector3.Distance(targetObjectTransform.position, attachTransform.position) < 0.001f)
+            {
+                isFollowing = false;
+            }
+        }
+    }
 
     private void LateUpdate()
     {
@@ -20,5 +44,11 @@ public class PickupHandler : MonoBehaviour
     public void toggleFollowing()
     {
         isFollowing = !isFollowing;
+    }
+
+    public void attachObjectToTransform()
+    {
+        isMovingToTransform = true;
+        isFollowing = false;
     }
 }
