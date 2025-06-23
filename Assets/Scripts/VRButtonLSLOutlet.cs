6d5ceead968c0eda2ed6396dfe9e0b0a118a688b
Rohan Modi <rohanmodi@outlook.in>
2025-06-23 15:58:49 -0400

    create lsl stream for headset/controller position

--- Assets/Scripts/VRButtonLSLOutlet.cs
+++ Assets/Scripts/VRButtonLSLOutlet.cs
@@ -1,9 +1,9 @@
+using LSL;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
-using LSL;
 using UnityEngine.SceneManagement;
-
 
 public class VRButtonLSLOutlet : MonoBehaviour
 {
@@ -12,7 +12,13 @@ public class VRButtonLSLOutlet : MonoBehaviour
     string StreamName = "VRStream";
     string StreamType = "Markers";
     private StreamOutlet outlet;
+    private StreamOutlet outlet2;
     private string[] sample = { "" };
+    private float[] positionSample = new float[9];
+
+    private Transform headTransform;
+    private Transform leftHandTransform;
+    private Transform rightHandTransform;
 
     private void Awake()
     {
@@ -27,6 +33,7 @@ public class VRButtonLSLOutlet : MonoBehaviour
 
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
+
     void Start()
     {
         var hash = new Hash128();
@@ -36,33 +43,80 @@ public class VRButtonLSLOutlet : MonoBehaviour
 
         StreamInfo streamInfo = new StreamInfo(StreamName, StreamType, 1, LSL.LSL.IRREGULAR_RATE, channel_format_t.cf_string, hash.ToString());
         outlet = new StreamOutlet(streamInfo);
-        Debug.Log("LSL stream created: " + StreamName);
+        //Debug.Log("LSL stream created: " + StreamName);
+
+        var hash2 = new Hash128();
+        hash2.Append("VRPositionStream");
+        hash2.Append("PositionStream");
+        hash2.Append(gameObject.GetInstanceID());
+
+        StreamInfo streamInfo2 = new StreamInfo("VRPositionStream", "PositionStream", 9, LSL.LSL.IRREGULAR_RATE, channel_format_t.cf_float32, hash2.ToString());
+        outlet2 = new StreamOutlet(streamInfo2);
+        //Debug.Log("LSL position stream created: " + "VRPositionStream");
     }
 
-    // called once per frame
+    void Update()
+    {
+        if (headTransform && leftHandTransform && rightHandTransform)
+        {
+            Vector3 h = headTransform.position;
+            Vector3 l = leftHandTransform.position;
+            Vector3 r = rightHandTransform.position;
+
+            positionSample[0] = h.x;
+            positionSample[1] = h.y;
+            positionSample[2] = h.z;
+
+            positionSample[3] = l.x;
+            positionSample[4] = l.y;
+            positionSample[5] = l.z;
+
+            positionSample[6] = r.x;
+            positionSample[7] = r.y;
+            positionSample[8] = r.z;
+
+            outlet2?.push_sample(positionSample);
+        }
+    }
+
     public void SendMarker(string label)
     {
         if (outlet != null)
         {
             sample[0] = label;
             outlet.push_sample(sample);
-            Debug.Log("LSL marker sent: " + label);
+            //Debug.Log("LSL marker sent: " + label);
         }
         else
         {
-            Debug.Log("LSL outlet not initialized");
+            //Debug.Log("LSL outlet not initialized");
         }
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        Debug.Log("Scene loaded: " + scene.name);
+        //Debug.Log("Scene loaded: " + scene.name);
         SendMarker("Scene Entered: " + scene.name);
+
+        headTransform = GameObject.Find("CenterEyeAnchor")?.transform;
+        leftHandTransform = GameObject.Find("LeftHandAnchor")?.transform;
+        rightHandTransform = GameObject.Find("RightHandAnchor")?.transform;
     }
 
     private void OnDestroy()
     {
+        if (outlet != null)
+        {
+            outlet.Close();
+            outlet = null;
+        }
+
+        if (outlet2 != null)
+        {
+            outlet2.Close();
+            outlet2 = null;
+        }
+
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
-
 }
